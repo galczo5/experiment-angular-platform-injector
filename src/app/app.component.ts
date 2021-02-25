a81b7d2609bec02c6b5dffdd457c39a5e6654991
galczo5 <kamil.galek@gmail.com>
2021-02-25 23:16:01 +0100

    poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
+import {interval} from 'rxjs';
+import {take} from 'rxjs/operators';
+
+const key = 'provided';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="d-flex align-items-center">
+      <button class="btn btn-lg btn-primary" (click)="provide()">Provide value</button>
+      <app-indicator class="ml-3"></app-indicator>
+    </div>
+    <i class="text-muted mt-3 d-block" *ngIf="needRefresh">Refresh to reload platform, refresh in {{ refreshIn }}s</i>
+  `
 })
-export class AppComponent {
-  title = 'angular-platform-injector';
+export class AppComponent implements OnInit {
+
+  provided: boolean;
+  needRefresh = false;
+  refreshIn = 3;
+
+  constructor(private readonly injector: Injector) {
+    console.log(injector);
+  }
+
+  ngOnInit(): void {
+    this.provided = !!localStorage.getItem(key);
+  }
+
+  provide(): void {
+
+    if (this.needRefresh) {
+      return;
+    }
+
+    this.needRefresh = true;
+
+    const item = localStorage.getItem(key);
+
+    if (item) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, 'true');
+    }
+
+    interval(1000)
+      .pipe(take(3))
+      .subscribe(() => {
+        this.refreshIn = this.refreshIn - 1;
+        if (this.refreshIn === 0) {
+          window.location.reload();
+        }
+      });
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {PlatformProviderService} from './app/platform-provider.service';
+import {TOKEN} from './app/token';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+const platformRef = platformBrowserDynamic();
+const item = localStorage.getItem('provided');
+
+if (item) {
+  console.log('Replace platform injector');
+  const injector = PlatformProviderService.getInjector(platformRef.injector, { provide: TOKEN, useValue: 'Injected with PlatformProviderService' });
+
+  // @ts-ignore
+  platformRef._injector = injector;
+}
+
+
+platformRef.bootstrapModule(AppModule)
   .catch(err => console.error(err));
